18c1e11ee8fc209e46530ed2d66982d57c30d02e
MRJ <mrjyuhongjiang@gmail.com>
2017-11-23 14:09:35 +0800

    0.2.3.0

--- Example/OC-Category/YHJViewController.m
+++ Example/OC-Category/YHJViewController.m
@@ -8,6 +8,9 @@
 
 #import "YHJViewController.h"
 //#import <OC_Category/Categories.h>
+#import <OC_Category/UIKit.h>
+
+#define SCREE_SIZE [UIScreen mainScreen].bounds.size
 
 @interface YHJViewController ()
 
@@ -18,11 +21,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-//    [[NSDate date] daysAgo];
-//    [[NSArray alloc] each:^(id object) {
-//        
-//    }];
-//    [[NSArray alloc] objectWithIndex:3];
+    
+    
+    UILabel *testLab = [[UILabel alloc] initWithFrame:CGRectMake(25, 100, SCREE_SIZE.width - 50, 160)];
+    testLab.text = @"数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....";
+    [testLab adjustLabelToMaximumSize:CGSizeMake(SCREE_SIZE.width - 50, 160) minimumFontSize:12];
+    testLab.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:testLab];
+    
+    
+    UILabel *test1Lab = [[UILabel alloc] initWithFrame:CGRectMake(testLab.left, testLab.bottom + 10, testLab.width, 160)];
+    test1Lab.text = @"数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....数据测试中....";
+    test1Lab.backgroundColor = [UIColor orangeColor];
+    [test1Lab adjustLabel];
+    [self.view addSubview:test1Lab];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
