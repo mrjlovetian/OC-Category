e0919abdaf60144f36650181ae9a49d3ffa5407f
MRJ <mrjyuhongjiang@gmail.com>
2017-11-24 08:48:08 +0800

    0.2.3.2

--- Example/OC-Category/YHJViewController.m
+++ Example/OC-Category/YHJViewController.m
@@ -13,7 +13,7 @@
 #define SCREE_SIZE [UIScreen mainScreen].bounds.size
 
 @interface YHJViewController ()
-
+@property (nonatomic, strong) UITextField *textField;
 @end
 
 @implementation YHJViewController
@@ -39,11 +39,35 @@
    
     test1Lab.backgroundColor = [UIColor orangeColor];
 //    [test1Lab adjustLabel];
-    [self.view addSubview:test1Lab];
+//    [self.view addSubview:test1Lab];
     test1Lab.numberOfLines = 0;
      [test1Lab setText:str automaticWritingAnimationWithBlinkingMode:UILabelAWBlinkingModeUntilFinish];
     
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(25, 150, SCREE_SIZE.width - 50, 16)];
+//    [self.textField setDidBeginEditingBlock:^(UITextField *textField) {
+//
+//    }];
+    self.textField.backgroundColor = [UIColor orangeColor];
+//    [self.view addSubview:self.textField];
+    
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 150, SCREE_SIZE.width - 50, 16)];
+//    textView.placeHolderTextView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, textView.frame.size.width, textView.frame.size.height)];
+    [textView addPlaceHolder:@"这是个占位符"];
+    [self.view addSubview:textView];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(30, 100, 60, 30);
+    btn.backgroundColor = [UIColor redColor];
+    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)click {
+    [self.textField shake];
+    
+    [self.view makeToast:@"吐司信息"];
 }
 
 - (void)didReceiveMemoryWarning
